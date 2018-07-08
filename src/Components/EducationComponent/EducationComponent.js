eeebe69dec293de6a313090b1f10e66569623f3e
vishu-chaudhary <vishal2421@outlook.com>
2018-07-08 19:43:08 +0530

    added category modal

--- src/Components/EducationComponent/EducationComponent.js
+++ src/Components/EducationComponent/EducationComponent.js
@@ -19,23 +19,69 @@ class Education extends Component {
             PHD,
             DIPLOMA
         ],
+        selectedEduType:null,
+        showCategories:false,
+        showEducationAddForm:false,
 
+    }
+    handleCategory = () =>{
+        if (!this.state.showCategories) {
+            // attach/remove event handler
+            document.addEventListener('click', this.handleOutsideClick, false);
+          } else {
+            document.removeEventListener('click', this.handleOutsideClick, false);
+          }
+        this.setState(prevState => ({
+            showCategories: !prevState.showCategories,
+         }));
+    }
+    handleOutsideClick = (event) => {
+        // ignore clicks on the component itself
+        if (this.node.contains(event.target)) {
+          return;
+        }
+        
+        this.handleCategory();
+      }
+    setCategory = (event) =>{
+        const selectedCategory = event.target.value
+        if (!this.state.showCategories) {
+            // attach/remove event handler
+            document.addEventListener('click', this.handleOutsideClick, false);
+          } else {
+            document.removeEventListener('click', this.handleOutsideClick, false);
+          }
+        this.setState(prevState => ({
+            showCategories: !prevState.showCategories,
+            selectedEduType: selectedCategory,
+            showEducationAddForm: true,
+         }));
     }
     render(){
         return(
-            <div className = {classes.education}>
+            <div className = {classes.education} >
                 <div className = {classes.eduHeading}>
                     <div className={classes.heading}>
                         <strong>EDUCATION:</strong> 
                         </div>
-                    <div className={classes.icon}>
-                        <i className = "fas fa-plus fa-1x"></i>
+                    <div className={classes.icon} >
+                        <i className = "fas fa-plus fa-1x" onClick={this.handleCategory}></i>
                         </div>
                 </div>
                 <hr/>
-                <div className = 'eduContent'>
-                    {this.state.eduDetails}
-                </div>    
+                { this.state.showCategories &&
+                    (
+                        <div className = {classes.modal}  >
+                            <div className={classes.modal_content}ref={node => { this.node = node; }}>
+                                {
+                                    this.state.eduDetails.map(
+                                    (category) => <button className={classes.button} onClick={this.setCategory} value={category}>{category}</button>
+                                    )
+                                }
+                            </div>
+                        </div>
+                    )
+                }   
             </div>
         )
     }
