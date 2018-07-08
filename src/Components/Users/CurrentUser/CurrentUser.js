5dd69d178440d0613fee02d82340c8d96f1fa1d6
vishu-chaudhary <vishal2421@outlook.com>
2018-07-08 16:29:13 +0530

    github link added

--- src/Components/Users/CurrentUser/CurrentUser.js
+++ src/Components/Users/CurrentUser/CurrentUser.js
@@ -81,7 +81,7 @@ class CurrentUser extends Component{
                     <div>{this.props.designation} at <span> {this.props.currentOrganization}</span></div>
                     <div className={classes.icon}>
                         <a href={this.props.linkedinLink} className={classes.linkedin}><i className="fab fa-linkedin fa-2x"></i></a>
-                        <a href={this.props.linkedinLink} className={classes.github}><i className="fab fa-github fa-2x"></i></a><br/>
+                        <a href={this.props.githubLink} className={classes.github}><i className="fab fa-github fa-2x"></i></a><br/>
                         <a href={"mailto:"+this.props.email} className={classes.email}><i className="fas fa-envelope fa-1x"></i><span>{this.props.email}</span></a>
                     </div>
                 </div>
